e2332b50c8b6ead295b735f8eae51f941cbe8fa0
armandopato <armauv@gmail.com>
2021-05-20 20:18:10 -0500

    Add logout button

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Button, chakra, Flex, Heading, IconButton, Menu, MenuButton, MenuItem, MenuList, Stack} from "@chakra-ui/react";
 import MascotHomeIcon from "../util/icons/MascotHomeIcon";
-import {signinRedirect} from "../auth/userService";
+import userManager, {signinRedirect, signoutRedirect} from "../auth/userService";
 import {HamburgerIcon} from "@chakra-ui/icons";
+import {User} from "oidc-client";
 
 function Header()
 {
+    const [user, setUser] = useState<User | null>(null);
+    useEffect(() => {
+        userManager.getUser().then((fetchedUser) => {
+            setUser(fetchedUser);
+        });
+    }, []);
+
     return (
         <chakra.header display="flex" justifyContent="center" padding={["1.3% 0", "1.5% 0", "1.6% 0", "1.2% 0"]}>
             <Flex justifyContent="space-between" alignItems="center" width={["94%", "90%", "80%"]}>
@@ -17,6 +25,10 @@ function Header()
                 </Flex>
 
                 <Stack direction="row" spacing={4} display={["none", "none", "flex"]}>
+                    {user && <Button colorScheme="red" variant="outline" onClick={signoutRedirect}>
+                        Cerrar sesión
+                    </Button>}
+
                     <Button colorScheme="blue" variant="solid" onClick={signinRedirect}>
                         Inicia sesión
                     </Button>
