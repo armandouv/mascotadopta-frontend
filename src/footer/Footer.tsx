048bd381672091aa745f9251849f0aab0679a079
armandopato <armauv@gmail.com>
2021-05-29 11:12:14 -0500

    Add empty adoptions handling

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -5,19 +5,17 @@ import MascotHomeIcon from "../util/icons/MascotHomeIcon";
 function Footer()
 {
     return (
-        <chakra.footer display="flex" justifyContent="center" padding="2.5% 0">
-            <Flex justifyContent="space-between" alignItems="center" width="92%">
-                <Flex>
-                    <MascotHomeIcon h={4} w={4} marginRight="12px"/>
-                    <Heading size="md">Mascotadopta</Heading>
-                </Flex>
+        <chakra.footer display="flex" alignItems="center" flexDir="column" padding="2.5% 0">
 
-                <Box>
-                    Contacto: mascotadopta@gmail.com
-                </Box>
-
-
+            <Flex>
+                <MascotHomeIcon h={4} w={4} marginRight="12px"/>
+                <Heading size="md">Mascotadopta</Heading>
             </Flex>
+
+            <Box mt="0.8em" color="gray.600">
+                <chakra.strong>Contacto:</chakra.strong>
+                mascotadopta@gmail.com
+            </Box>
         </chakra.footer>
     );
 }
--- src/main/pets/PetsInfoContainer.tsx
+++ src/main/pets/PetsInfoContainer.tsx
@@ -1,6 +1,6 @@
 import PetInfo, {PetInfoDto} from "./PetInfo";
 import React, {useState} from "react";
-import {Grid} from "@chakra-ui/react";
+import {Box, Grid, Text} from "@chakra-ui/react";
 
 const testPetInfo: PetInfoDto = {
     createdDate: new Date(),
@@ -13,17 +13,21 @@ const testPetInfo: PetInfoDto = {
     imageAlt: "Perrito"
 };
 
-const pets = Array(10).fill(testPetInfo);
+const pets: PetInfoDto[] = Array(9).fill(testPetInfo);
 
 function PetsInfoContainer()
 {
     const [petsInfo, setPetsInfo] = useState<PetInfoDto[]>(pets);
+    if (petsInfo.length === 0)
+        return (<Box minHeight="40rem" display="flex" alignItems="center">
+            <Text fontSize="4xl">Aún no hay ninguna mascota en adopción ):</Text>
+        </Box>);
+
     const petInfoComponents = petsInfo.map(petInfo => {
         return <PetInfo key={petInfo.id} {...petInfo} />
     });
-
     return (
-        <Grid maxWidth={["80%"]} templateColumns="repeat(3, 1fr)" gap={8}>
+        <Grid minHeight="40rem" maxWidth={["80%"]} templateColumns="repeat(3, 1fr)" gap={8}>
             {petInfoComponents}
         </Grid>
     );
